78c957128ed1f3884d0cae5af235dc406c179d63
oladevop <161063026+oladevop@users.noreply.github.com>
2025-05-29 06:41:45 -0700

    user.py Updated

--- user.py
+++ user.py
@@ -4,3 +4,4 @@ this is a bash shell script
 print ('$name Welcome To Visa')
 echo "bash shell scripting is always position this way"
 echo "scripting is automation"
+# We must always use comment to give clarity to the code which we have written bcos of Non IT Guy
